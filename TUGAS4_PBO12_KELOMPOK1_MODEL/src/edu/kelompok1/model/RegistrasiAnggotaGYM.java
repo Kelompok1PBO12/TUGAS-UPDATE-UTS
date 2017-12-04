9c23299d6cc423b5f8c2ad78cde73e1e65e12726
sthevan <32483662+sthevan@users.noreply.github.com>
2017-12-04 15:47:08 +0700

    Add files via upload

---
+++ TUGAS4_PBO12_KELOMPOK1_MODEL/src/edu/kelompok1/model/RegistrasiAnggotaGYM.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kelompok1.model;
+
+/**
+ *
+ * @author Sthevan 10116541
+ */
+public class RegistrasiAnggotaGYM {
+    private String noKTP;
+     private String nama;
+     private String jenisKelamin;
+     private String cabang;
+     private String handuk;
+     private String paketFitnes;
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public void setNoKTP(String noKTP) {
+        this.noKTP = noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getPaketFitnes() {
+        return paketFitnes;
+    }
+
+    public void setPaketFitnes(String paketFitnes) {
+        this.paketFitnes = paketFitnes;
+    }
+     
+}
---
+++ TUGAS4_PBO12_KELOMPOK1_MODEL/src/edu/kelompok1/model/TUGAS4_PBO12_KELOMPOK1_MODEL.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kelompok1.model;
+
+/**
+ *
+ * @author user
+ */
+public class TUGAS4_PBO12_KELOMPOK1_MODEL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
